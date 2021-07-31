69b1ccd6fd2ee85ffac6f4a8b8b14174e7e02469
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2021-07-31 20:42:07 +0200

    basic demo

--- LoopingTransform/ViewController.swift
+++ LoopingTransform/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var rotatingView: UIView!
+    private var angle: CGFloat = 0
+    private let speed: CGFloat = 0.05
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        CADisplayLink(target: self, selector: #selector(handleDisplayLink)).add(to: .main, forMode: .common)
     }
-
-
+    
+    @objc private func handleDisplayLink() {
+        rotatingView.layer.transform = CATransform3DMakeRotation(angle, 0, 1, 0)
+        angle = (angle + speed).truncatingRemainder(dividingBy: .pi * 2)
+    }
 }
 
