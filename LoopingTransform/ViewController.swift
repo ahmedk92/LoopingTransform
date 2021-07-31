e681cafb8fed68c30b34739f1a7a65dbc6fda46b
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2021-07-31 21:09:20 +0200

    Add CABasicAnimation example.

--- LoopingTransform/ViewController.swift
+++ LoopingTransform/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet private weak var rotatingView: UIView!
+    @IBOutlet private weak var rotatingView1: UIView!
+    @IBOutlet private weak var rotatingView2: UIView!
     private var angle: CGFloat = 0
     private let speed: CGFloat = 0.05
 
@@ -18,10 +19,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         CADisplayLink(target: self, selector: #selector(handleDisplayLink)).add(to: .main, forMode: .common)
+        
+        let animation = CABasicAnimation(keyPath: "transform.rotation.y")
+        animation.fromValue = 0
+        animation.toValue = CGFloat.pi * 2
+        animation.repeatCount = .infinity
+        animation.speed = Float(speed)
+        rotatingView2.layer.add(animation, forKey: "rotate")
     }
     
     @objc private func handleDisplayLink() {
-        rotatingView.layer.transform = CATransform3DMakeRotation(angle, 0, 1, 0)
+        rotatingView1.layer.transform = CATransform3DMakeRotation(angle, 0, 1, 0)
         angle = (angle + speed).truncatingRemainder(dividingBy: .pi * 2)
     }
 }
